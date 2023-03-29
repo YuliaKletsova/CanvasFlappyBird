8705d949771166a9c4cd625dd0dd44bf5b2cca50
Юлия Клецова <yvkletsova@sbercloud.ru>
2023-03-29 23:44:23 +0300

    adding crash rules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canvas');
+const button = document.getElementById('button');
 const ctx = canvas.getContext('2d');
 
+// - - - - - - - - - - Images - - - - - - - - - - - - - - -
 var bird = new Image();
 bird.src = 'static/flappy_bird_bird.png';
 var bg = new Image();
@@ -11,81 +13,86 @@ var pipeBottom = new Image();
 pipeBottom.src = 'static/flappy_bird_pipeBottom.png';
 var footer = new Image();
 footer.src = 'static/flappy_bird_footer.png';
-// - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
 var x = 10;
 var y = (canvas.height - footer.height) / 2;
 var score = 0;
 var g = 0.5;
 var pipeSpeed = 1;
-var verticalGap = 90, horizontalGap = 100;
+var verticalGap = 90,
+  horizontalGap = 100;
 var animationFrameCounter;
+var abort = false;
 
-const ORIENTATION = {
-    top: 'top',
-    bottom: 'bottom',
-    both: 'both',
-}
-
-function goUp() {
-    y -=20;
-}
-
-var pipes = [{
-    orientation: ORIENTATION.both,
+var pipes = [
+  {
     x: canvas.width,
-    y: 0
-}];
+    y: 0,
+  },
+];
 
-function drawPipes() {
-    for(let pipe of pipes) {
-        ctx.drawImage(pipeTop, pipe.x, pipe.y);
-        ctx.drawImage(pipeBottom, pipe.x, pipe.y + pipeTop.height + verticalGap);
-
-        pipe.x -= pipeSpeed;
-
-        if (pipe.x < horizontalGap + 1 && pipe.x > horizontalGap - 1) {
-            pipes.push({
-                x : canvas.width,
-                y : Math.floor(Math.random() * pipeTop.height) - pipeTop.height
-            });
-        }
-
-    }
+function drawAbortScreen() {
+    ctx.fillStyle = 'rgb(224,224,224,0.7)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillText('GAMEOVER', canvas.width / 2 - 70, canvas.height / 2);
 }
 
 function drawGame() {
-    ctx.drawImage(bg, 0, 0);
-    // switch(score) {
-    //     case score > 5:
-    //         pipeSpeed = 2;
-    //         break;
-    //     case score > 10:
-    //         pipeSpeed = 5;
-    //         break;
-    //     default:
-    //         pipeSpeed = 1;
-    //         break;
-    // }
+  ctx.drawImage(bg, 0, 0);
 
-    drawPipes();
+  for (let pipe of pipes) {
+    ctx.drawImage(pipeTop, pipe.x, pipe.y);
+    ctx.drawImage(pipeBottom, pipe.x, pipe.y + pipeTop.height + verticalGap);
 
-    ctx.drawImage(bird, x, y);
-    ctx.drawImage(footer, 0, canvas.height - footer.height);
+    pipe.x -= pipeSpeed;
 
-    ctx.fillStyle = "#000";
-    ctx.font = "24px Verdana";
-    ctx.fillText("Счет: " + score, 10, canvas.height - 20);
-
-    if (y <= 0 || y >= canvas.height - footer.height - bird.height - g) {
-        cancelAnimationFrame(animationFrameCounter);
-        return;
-    } else {
-        y += g;
+    if (
+      (x + bird.width >= pipe.x &&
+        x <= pipe.x + pipeTop.width &&
+        (y <= pipe.y + pipeTop.height || y + bird.height >= pipe.y + pipeTop.height + verticalGap)) ||
+      y + bird.height >= canvas.height - footer.height
+    ) {
+      abort = true;
+    } else if (x === pipe.x + pipeTop.width + 1) {
+      score++;
     }
 
-    animationFrameCounter = requestAnimationFrame(drawGame);
+    if (pipe.x < horizontalGap + 1 && pipe.x > horizontalGap - 1) {
+      pipes.push({
+        x: canvas.width,
+        y: Math.floor(Math.random() * pipeTop.height) - pipeTop.height,
+      });
+    }
+  }
+
+  ctx.drawImage(bird, x, y);
+  ctx.drawImage(footer, 0, canvas.height - footer.height);
+  ctx.fillStyle = '#000';
+  ctx.font = '24px Verdana';
+  ctx.fillText('Счет: ' + score, 10, canvas.height - 20);
+
+  // - - - - - - Dealing with top & bottom touches - - - - - - - - - -
+  if (y <= 0 || y >= canvas.height - footer.height - bird.height - g) {
+    abort = true;
+  } else {
+    y += g;
+  }
+
+  if (abort) {
+    drawAbortScreen();
+    cancelAnimationFrame(animationFrameCounter);
+    return;
+  }
+  animationFrameCounter = requestAnimationFrame(drawGame);
 }
 
-document.addEventListener('keydown', goUp);
+document.addEventListener('keydown', () => {
+  y -= 20;
+});
+
+button.addEventListener('click', () => {
+    location.reload();
+})
 
 drawGame();
